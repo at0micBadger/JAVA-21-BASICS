6e22e08ec2e9e39ae8739c93195befa5e6384b9a
at0micBadger <at0mic_badger@yahoo.com>
2026-02-16 11:08:15 -0500

    Add files via upload

---
+++ BeVariable.java
@@ -0,0 +1,38 @@
+public class BeVariable {
+    public static void main(String[] args) {
+
+        System.out.println("");
+        System.out.println("Here are some printed variables.");
+        System.out.println("");
+
+
+        // STRING VARIABLES
+        String name_1 = "Jack";
+        String name_2 = "Jill";
+
+        System.out.println("name_1: " + name_1);
+        System.out.println("name_2: " + name_2);
+
+        // CHAR VARIABLE
+        char answer = 'Y';
+        System.out.println("answer: " + answer);
+
+         // INTEGER (whole number)
+        int score = 42;
+
+        // FLOATING POINT
+        double price = 9.99;
+
+        System.out.println("score: " + score);
+        System.out.println("price: " + price);
+
+        // BOOLEAN
+        boolean isRaining = true;
+
+        System.out.println("score: " + score);
+        System.out.println("price: " + price);
+        System.out.println("isRaining: " + isRaining);
+
+        System.out.println("");
+    }
+}
