93f0eb21601234eaa03349e031a28505108f3259
at0micBadger <at0mic_badger@yahoo.com>
2026-03-02 10:59:57 -0500

    Add files via upload

---
+++ MathOpsZ.java
@@ -0,0 +1,82 @@
+public class MathOpsZ {
+    public static void main(String[] args) {
+        // PROGRAM START
+        System.out.println("");
+        System.out.println("START PROGRAM.");
+        System.out.println("");
+        
+        System.out.println("Testing Math Operators:");
+        System.out.println("");
+
+        // TEST ADDITION OPERATORS
+        System.out.println("testing addition operators:");
+        int result = 2 + 7;
+        System.out.println("addition result:  " + result);
+
+        double nextSet = 3.14 + 99.99;
+        System.out.println("addition of next set:  " + nextSet);
+
+        System.out.println("");
+
+        //TEST SUBTRACTION OPERATORS
+        System.out.println("testing subtraction operators:");
+        int numTarget = 3 - 44;
+        System.out.println("numeric target: " + numTarget);
+
+        double nextTarget = 2.14 - 1.00;
+        System.out.println("next target: " + nextTarget);
+
+        System.out.println("");
+
+        //TEST MULTIPLICATION OPERATORS
+        System.out.println("testing multiplication operators:");
+        int productZ = 9 * 9;
+        System.out.println("product Z: " + productZ);
+
+        double targetY = 3.14 * 9.99;
+        System.out.println("target Y: " + targetY);
+
+        System.out.println("");
+
+        //TEST DIVISION OPERATORS
+        System.out.println("testing division operators:");
+        int storedValue = 9 / 3;
+        System.out.println("stored value 1: " + storedValue);
+
+        double storedValueX = 99.99 / 3.14;
+        System.out.println("stored value X: " + storedValueX);
+
+        System.out.println("");
+
+        //TEST MODULUS OPERATORS
+        System.out.println("testing modulus operators:");
+        int I = 9;
+        int j = 3;
+        int superMod = j % I;
+        System.out.println("The modulus of the values: " + superMod);
+
+        System.out.println("");
+
+        //TEST INCREMENT OPERATORS
+        System.out.println("testing increment operators:");
+        int x = 8;
+        int y = ++x;
+        System.out.println("The increment of x is: " + y);
+
+        System.out.println("");
+
+        //TEST DECREMENT OPERATORS
+        System.out.println("testing decrement operators:");
+        int f = 37;
+        int g = --f;
+        System.out.println("The decrement of the number is: " + g);
+
+        System.out.println("");
+
+        //PROGRAM END
+        System.out.println("END PROGRAM.");
+        System.out.println("");   
+        
+    }
+}
+
